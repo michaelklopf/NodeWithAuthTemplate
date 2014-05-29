5549da68a1ddba0322a4599b65e6a621818a12d5
Michael Klopf <klopf.michael@gmail.com>
2014-05-29 10:36:19 +0200

    Upgraded to Express 4.

--- web.js
+++ web.js
@@ -7,6 +7,10 @@ var stylus      = require('stylus');
 var mongoose    = require('mongoose');
 var passport    = require('passport');
 var flash       = require('connect-flash');
+var cookieParser  = require('cookie-parser');
+var bodyParser    = require('body-parser');
+var methodOverride = require('method-override');
+var session     = require('express-session');
 
 // load config and create database
 var database    = require('./config/database');
@@ -17,31 +21,27 @@ require('./config/passport.js')(passport);
 // create and configure express app
 var app = express();
 
-app.configure(function() {
-    app.use(express.cookieParser('secretpassauth'));
-    //app.use(express.bodyParser()); // get rid of dep messages with following two lines
-    app.use(express.json());
-    app.use(express.urlencoded());
-    app.use(express.session({ key: 'session', cookie: { maxAge: 60000 }}));
-	  app.use(express.methodOverride());
 
-    app.use(stylus.middleware(__dirname + '/static'));
+app.use(cookieParser('secretpassauth'));
+app.use(bodyParser());
+app.use(session({ key: 'session', cookie: { maxAge: 60000 }}));
+app.use(methodOverride());
 
-    app.use("/css" , express.static(__dirname + '/static/css'));
-    app.use("/js" , express.static(__dirname + '/static/js'));
-    app.use("/fonts", express.static(__dirname + '/static/fonts'));
+app.use(stylus.middleware(__dirname + '/static'));
 
-    app.set('views', __dirname + '/app/views');
-    app.engine('jade', require('jade').__express);
-    app.set('view engine', 'jade');  // revert to ejs with replacing jade -> ejs and comment the above line
-    app.set('port', process.env.PORT || 8080);
+app.use("/css" , express.static(__dirname + '/static/css'));
+app.use("/js" , express.static(__dirname + '/static/js'));
+app.use("/fonts", express.static(__dirname + '/static/fonts'));
 
-    app.use(passport.initialize());
-    app.use(passport.session());
+app.set('views', __dirname + '/app/views');
+app.engine('jade', require('jade').__express);
+app.set('view engine', 'jade');  // revert to ejs with replacing jade -> ejs and comment the above line
+app.set('port', process.env.PORT || 8080);
 
-    app.use(flash());
-    app.use(app.router);
-});
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 // load routes
 require('./app/routes.js')(app, passport);
